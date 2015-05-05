c359ab61dbc0c6ec44e1a1273dc0b8bc88f1cc72
Xin <xin@turboholdings.com>
2015-05-05 15:31:57 -0400

    Initial Commit

---
+++ SalaryHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SalaryHelper
+//
+//  Created by Turbo on 5/5/15.
+//  Copyright (c) 2015 Xin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SalaryHelper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SalaryHelper
+//
+//  Created by Turbo on 5/5/15.
+//  Copyright (c) 2015 Xin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
